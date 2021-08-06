fa99476069252fff8faf52094d7c643c3155f2f0
TOPans <TOPans@mail.ru>
2021-08-06 16:46:01 +0300

    feat: implement function check brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let config = bracketsConfig.map(elem => elem.join(""));    
+    for (let i = 0; i < config.length; i++) {
+        if (str.includes(config[i])) {
+            str = str.replace([config[i]], "");
+            i = -1;
+        }
+    }
+    return !str.length;
+};
+
